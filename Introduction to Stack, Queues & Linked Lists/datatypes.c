1120c1cba39bed40156b2057de0923193b6059a7
Shaikh Mudassir <send2mudassir@gmail.com>
2022-10-29 10:07:13 +0000

    data types

--- Introduction to Stack, Queues & Linked Lists/datatypes.c
+++ Introduction to Stack, Queues & Linked Lists/datatypes.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
-#include<conio.h>
-void main() {
 
+void main() {
+    int no;
+    char str[10];
+
+    printf("Enter a charachter: ");
+    scanf("%s",&str);
+
+    printf("Enter a number: ");
+    scanf("%d",&no);
+
+    printf("\n Entered Integer is %d and Entered Charachter is %s",no,str);
+
+    
 }
